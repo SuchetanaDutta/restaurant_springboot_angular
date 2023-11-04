c0fae58c58e281d24eed75a2ce6b6e3a01a1a287
Suchu500 <suchetanad100@gmail.com>
2023-11-04 10:47:35 +0530

    3

---
+++ src/app/modules/admin/admin-services/admin.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StorageService } from 'src/app/auth-services/storage-service/storage.service';
+
+
+const BASIC_URL = ["https://localhost:8000/"]
+
+@Injectable({
+    providedIn: 'root'
+}) 
+export class AdminService {
+    constructor(private http: HttpClient) {}
+
+    postCategory(categoryDto: any):Observable<any>{
+        console.log(this.createAuthorizationHeader())
+        return this.http.post<[]>(BASIC_URL + "api/admin/category", categoryDto,
+        {
+            headers:this.createAuthorizationHeader()
+        }
+        )
+    }
+
+    getAllCategories():Observable<any>{
+        return this.http.post<[]>(BASIC_URL + "api/admin/category", 
+        {
+            headers:this.createAuthorizationHeader()
+        }
+        )
+    }
+
+    getAllCategoriesByTitle(title:string):Observable<any>{
+        return this.http.post<[]>(BASIC_URL + 'api/admin/category/${title}', 
+        {
+            headers:this.createAuthorizationHeader()
+        }
+        )
+    }
+
+    createAuthorizationHeader():HttpHeaders{
+        let authHeaders:HttpHeaders = new HttpHeaders();
+        return authHeaders.set(
+            "Authorization", "Bearer " + StorageService.getToken()
+        );
+    }
+}
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,17 +1,41 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { AdminRoutingModule } from './admin-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
+import { addCategoryComponent } from './admin-components/add-category/add-category.component';
+import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from './admin-routing.module';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    addCategoryComponent
   ],
   imports: [
+    BrowserModule,
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzDropDownModule, 
+    NzDividerModule,
+    NzGridModule
   ]
 })
 export class AdminModule { }
